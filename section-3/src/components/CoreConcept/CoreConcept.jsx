080da74a284c36fbf79a47b4dd7d1d0c11ab8478
writecody <n/a>
2024-08-08 17:36:48 -0500

    add components folder and css files

---
+++ section-3/src/components/CoreConcept/CoreConcept.jsx
@@ -0,0 +1,11 @@
+import "./CoreConcept.css";
+
+export default function CoreConcept({ description, image, title }) {
+    return (
+        <li>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+    );
+}
