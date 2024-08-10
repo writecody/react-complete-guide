1a26e659c8172429ebb3f90f28768a29dcbae548
writecody <36807056+writecody@users.noreply.github.com>
2024-08-10 16:19:49 -0500

    add two-way binding for player name field

--- section-4/src/components/Player/Player.jsx
+++ section-4/src/components/Player/Player.jsx
@@ -1,11 +1,34 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing((currentValue) => !currentValue);
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+    }
+
     return (
         <li>
-            <span className="player">
-                <span className="player-name">{name}</span>
-                <span className="player-symbol">{symbol}</span>
-            </span>
-            <button>Edit</button>
+            <>
+                <span className="player">
+                    {editablePlayerName}
+                    <span className="player-symbol">{symbol}</span>
+                </span>
+                <button onClick={handleEditClick}>
+                    {isEditing ? "Save" : "Edit"}
+                </button>
+            </>
         </li>
     );
 }
