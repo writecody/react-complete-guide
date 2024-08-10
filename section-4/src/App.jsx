dce1a6bf9d2f76bbd3b9e934ea4b38a5b7d493b5
writecody <n/a>
2024-08-10 15:15:29 -0500

    add Player component

--- section-4/src/App.jsx
+++ section-4/src/App.jsx
@@ -1,9 +1,16 @@
+import Player from "./components/Player/Player"
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player1" symbol="X" />
+          <Player name="Player2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
   )
 }
 
