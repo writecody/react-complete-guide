825a0613c9115b4f5abd8ba66beca0f714b352fb
writecody <36807056+writecody@users.noreply.github.com>
2024-08-17 14:47:19 -0500

    show score

--- react-refs-and-portals/src/components/ResultModal.jsx
+++ react-refs-and-portals/src/components/ResultModal.jsx
@@ -5,6 +5,7 @@ const ResultModal = forwardRef(function ResultModal({ remainingTime, targetTime,
 
     const userLost = remainingTime <= 0;
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
     useImperativeHandle(ref, () => {
         return {
@@ -17,6 +18,7 @@ const ResultModal = forwardRef(function ResultModal({ remainingTime, targetTime,
     return (
         <dialog ref={dialog} className="result-modal">
             {userLost && <h2>You lost...</h2>}
+            {!userLost && <h2>Your score: {score}</h2>}
             <p>The target time was <strong>{targetTime} seconds...</strong></p>
             <p>You stopped the timer with <strong>{formattedRemainingTime} left</strong></p>
             <form method="dialog" onSubmit={onReset}>
