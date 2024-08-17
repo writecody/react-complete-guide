debde042dbfd282d4467f96e724cba2005ee77d3
writecody <36807056+writecody@users.noreply.github.com>
2024-08-17 14:48:19 -0500

    reset score on close modal

--- react-refs-and-portals/src/components/ResultModal.jsx
+++ react-refs-and-portals/src/components/ResultModal.jsx
@@ -16,7 +16,7 @@ const ResultModal = forwardRef(function ResultModal({ remainingTime, targetTime,
     });
 
     return (
-        <dialog ref={dialog} className="result-modal">
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
             {userLost && <h2>You lost...</h2>}
             {!userLost && <h2>Your score: {score}</h2>}
             <p>The target time was <strong>{targetTime} seconds...</strong></p>
