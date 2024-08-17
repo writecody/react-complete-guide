6a3aee326da7bffffd1a20d2188dcbc07a13df87
writecody <36807056+writecody@users.noreply.github.com>
2024-08-17 14:08:10 -0500

    add useImperativeHandle hook

--- react-refs-and-portals/src/components/ResultModal.jsx
+++ react-refs-and-portals/src/components/ResultModal.jsx
@@ -1,8 +1,18 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+    const dialog = useRef();
+
+    useImperativeHandle(ref, () => {
+        return {
+            openModal() {
+                dialog.current.showModal();
+            }
+        };
+    });
+
     return (
-        <dialog ref={ref} className="result-modal">
+        <dialog ref={dialog} className="result-modal">
             <h2>You {result}</h2>
             <p>The target time was <strong>{targetTime} seconds...</strong></p>
             <p>You stopped the timer with <strong>X seconds left.</strong></p>
--- react-refs-and-portals/src/components/TimerChallenge.jsx
+++ react-refs-and-portals/src/components/TimerChallenge.jsx
@@ -11,7 +11,7 @@ export default function TimerChallenge({ title, targetTime }) {
     function handleStart() {
         timer.current = setTimeout(() => {
             setTimerExpired(true);
-            dialog.current.showModal();
+            dialog.current.openModal();
         }, targetTime * 1000);
         
         setTimerStarted(true);
