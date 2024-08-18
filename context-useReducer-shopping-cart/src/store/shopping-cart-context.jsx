3cd5b32e6054e8644f44924a730dcdbc336f2c9b
writecody <36807056+writecody@users.noreply.github.com>
2024-08-18 16:42:04 -0500

    initialize ShoppingCartReducer

--- context-useReducer-shopping-cart/src/store/shopping-cart-context.jsx
+++ context-useReducer-shopping-cart/src/store/shopping-cart-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 import { DUMMY_PRODUCTS } from "../dummy-products";
 
 export const CartContext = createContext({
@@ -7,7 +7,15 @@ export const CartContext = createContext({
   updateItemQuantity: () => {},
 });
 
+function shoppingCartReducer(state, action) {
+  return state;
+}
+
 export default function CartContextProvider({ children }) {
+  const [shoppingCartState, shoppingCartDispactch] = useReducer(shoppingCartReducer, {
+    items: [],
+  });
+
   const [shoppingCart, setShoppingCart] = useState({
     items: [],
   });
@@ -69,7 +77,7 @@ export default function CartContextProvider({ children }) {
   }
 
   const ctxValue = {
-    items: shoppingCart.items,
+    items: shoppingCartState.items,
     addItemToCart: handleAddItemToCart,
     updateItemQuantity: handleUpdateCartItemQuantity,
   };
