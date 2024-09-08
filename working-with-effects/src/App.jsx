bac9b18f964631e75e7d1e8132156a44c7b15277
writecody <36807056+writecody@users.noreply.github.com>
2024-09-08 14:59:40 -0500

    display quiz questions and allow select answers

--- working-with-effects/src/App.jsx
+++ working-with-effects/src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
-
+import Quiz from "./components/Quiz";
 function App() {
-    return(
-        <Header></Header>
-    )
+  return (
+    <>
+      <Header></Header>
+      <main>
+        <Quiz></Quiz>
+      </main>
+    </>
+  );
 }
 
 export default App;
