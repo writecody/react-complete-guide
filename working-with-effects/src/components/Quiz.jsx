21318525e95e85c991a62edfdbe6b252fb73b3f8
writecody <36807056+writecody@users.noreply.github.com>
2024-09-08 15:08:59 -0500

    add quiz complete page

--- working-with-effects/src/components/Quiz.jsx
+++ working-with-effects/src/components/Quiz.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
 import QUESTIONS from "../questions";
+import QuizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswers) => {
@@ -13,12 +15,24 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={QuizCompleteImg} alt="Quiz complete image" />
+        <h2>Quiz Complete</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
